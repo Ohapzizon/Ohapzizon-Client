b8bd329091479bc31cbb06004a4aa59cef9918d8
JeongTaehwan <wjdxoghks11222@naver.com>
2021-12-20 09:11:32 +0900

    Update : 버튼 수정

--- Components/Button/Style.ts
+++ Components/Button/Style.ts
@@ -1,18 +1,12 @@
-import styled from 'styled-components';
-import Config from '../../Constants/Config.json';
-
-export const Positioner = styled.div`
-    width: 100%;
-    height: 100%;
-`;
+import styled from "styled-components";
+import Config from "../../Constants/Config.json";
 
 export const Button = styled.button`
-    width: 33.3%;
-    height: 5%;
-    border-radius: 5.5rem;
-    border: none;
-    font-size: 2.5rem;
-    font-weight: bold;
-    background-color: ${Config.COLOR.BLUE};
-    color: ${Config.COLOR.WHITE};
+  width: 27.3%;
+  height: 5%;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.7rem;
+  background-color: ${Config.COLOR.BLUE};
+  color: ${Config.COLOR.WHITE};
 `;
