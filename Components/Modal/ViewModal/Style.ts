1bef7553963254500c06984f1be4bc30542f37c1
JeongTaehwan <wjdxoghks11222@naver.com>
2021-12-25 16:12:51 +0900

    Add : ViewModal

--- Components/Modal/ViewModal/Style.ts
+++ Components/Modal/ViewModal/Style.ts
@@ -1,5 +1,47 @@
 import styled from "styled-components";
+import Config from "../../../Constants/Config.json";
 
-export const ModalWrapper = styled.div``;
+export const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
-export const NameInput = styled.input``;
+export const Title = styled.p`
+  font-size: 36px;
+  font-family: "Poppins";
+  font-weight: bold;
+  font-style: italic;
+  color: ${Config.COLOR.BLACK};
+`;
+
+export const People = styled.p`
+  margin-top: 0.5rem;
+  font-size: 16px;
+  font-family: "Poppins";
+  font-weight: bold;
+  font-style: italic;
+  color: ${Config.COLOR.BLACK};
+`;
+
+export const TextViewer = styled.div`
+  width: 40.8%;
+  height: 50vh;
+  margin-top: 1rem;
+  border-radius: 10px;
+  border: 1.5px solid #dbdbdb;
+`;
+
+export const Text = styled.p`
+  margin-top: 1rem;
+  margin-left: 0.5rem;
+  font-size: 16px;
+  font-family: "Poppins";
+  font-weight: bold;
+  font-style: italic;
+`;
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 13rem;
+  margin-left: 34%;
+  margin-top: 1.2vh;
+`;
--- Components/Modal/ViewModal/ViewModal.tsx
+++ Components/Modal/ViewModal/ViewModal.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import * as s from "./Style";
-import * as i from "../../../Assets/index";
+import TextInput from "../../Input/TextInput/TextInput";
+import ModalButton from "../../Button/ModalButton/ModalButton";
 
-const ViewModal = () => {
-  return <s.ModalWrapper></s.ModalWrapper>;
+const WriteModal = () => {
+  return (
+    <s.ModalWrapper>
+      <s.Title>점축 땡길 사람?</s.Title>
+      <s.People>20명</s.People>
+      <s.TextViewer>
+        <s.Text>
+          오늘 날씨가 정말 좋은데 이건 축구를 안 땡기면 범죄다 ㄹㅇ
+        </s.Text>
+      </s.TextViewer>
+      <s.ButtonWrapper>
+        <ModalButton text="땡겨!!" className="check" />
+      </s.ButtonWrapper>
+    </s.ModalWrapper>
+  );
 };
 
-export default ViewModal;
+export default WriteModal;
