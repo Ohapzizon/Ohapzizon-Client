88523be100e3c652d77afd9bee469e53e08abb91
JeongTaehwan <wjdxoghks11222@naver.com>
2021-12-27 15:45:35 +0900

    Add : Modal DummyDate

--- Components/Modal/ViewModal/ViewModal.tsx
+++ Components/Modal/ViewModal/ViewModal.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import * as s from "./Style";
 import ModalButton from "../../Button/ModalButton/ModalButton";
 
-const ViewModal = () => {
+type modal = {
+  id: number;
+  title: string;
+  people: number;
+  content: string;
+};
+
+interface ModalProps {
+  modalObj: modal;
+}
+
+const ViewModal: React.FC<ModalProps> = ({ modalObj }) => {
   return (
     <s.ModalWrapper>
-      <s.Title>점축 땡길 사람?</s.Title>
-      <s.People>20명</s.People>
+      <s.Title>{modalObj.title}</s.Title>
+      <s.People>{modalObj.people} 명</s.People>
       <s.TextViewer>
-        <s.Text>
-          오늘 날씨가 정말 좋은데 이건 축구를 안 땡기면 범죄다 ㄹㅇ
-        </s.Text>
+        <s.Text>{modalObj.content}</s.Text>
       </s.TextViewer>
       <s.ButtonWrapper>
         <ModalButton text="땡겨!!" className="check" />
