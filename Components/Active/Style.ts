3f0c76b80f18fd46eedebb3b0c13c6a33cde6a06
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-12-27 15:14:04 +0900

    Add : Active publishing

---
+++ Components/Active/Style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 100px;
+  justify-items: center;
+`;
