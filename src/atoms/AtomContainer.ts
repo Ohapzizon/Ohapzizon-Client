2fcb7ee644555e0e73d9408c643379dc296e4914
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-12-31 00:50:14 +0900

    Remove : atom값을 일단 지움

--- src/atoms/AtomContainer.ts
+++ src/atoms/AtomContainer.ts
@@ -1,14 +1,8 @@
 import { atom } from "recoil";
-import { CheckTime } from "../Util/CheckTime";
 
 export const isOpen = atom<boolean>({
   key: "isOpen",
   default: false,
-});
-
-export const theme = atom<string>({
-  key: "theme",
-  default: CheckTime(),
 });
 
 export const isWriteModalOpen = atom<boolean>({
