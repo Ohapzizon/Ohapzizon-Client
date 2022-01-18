17ba98ba539ac30011a6e28b2e9e37e181f89300
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-01-18 18:15:51 +0900

    Update : google Link를 받아와 버튼에 적용

--- src/Components/Button/GoogleButton/Googlebutton.tsx
+++ src/Components/Button/GoogleButton/Googlebutton.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import member from "../../../Api/member";
 import * as i from "../../../Assets/index";
 import * as s from "./Style";
 
 type ButtonProps = {
-  onClick: (e: any) => void;
+  onClick?: (e: any) => void;
 };
 
 const Googlebutton: React.FC<ButtonProps> = ({ onClick }) => {
+  const [googleLink, setGoogleLink] = useState<string>();
+
+  const onGoogleURL = async () => {
+    try {
+      const res = await member.googleAuth();
+      setGoogleLink(res.data);
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    onGoogleURL();
+  }, []);
   return (
     <>
-      <s.Button onClick={onClick}>
+      <s.Button href={googleLink}>
         <i.Google />
       </s.Button>
       <s.Text>Google 로그인</s.Text>
